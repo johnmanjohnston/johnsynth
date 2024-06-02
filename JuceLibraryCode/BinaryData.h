d0264d012e8b04d685e7a1d1fadc67d98539ad94
johnmanjohnston <c.ethanjohn@gmail.com>
2024-06-02 16:02:11 +0400

    moved Helvetica to assets/ and added Space Mono font

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -14,8 +14,11 @@ namespace BinaryData
     extern const char*   HelveticaLight_ttf;
     const int            HelveticaLight_ttfSize = 161716;
 
+    extern const char*   SpaceMonoRegular_ttf;
+    const int            SpaceMonoRegular_ttfSize = 90904;
+
     // Number of elements in the namedResourceList and originalFileNames arrays.
-    const int namedResourceListSize = 2;
+    const int namedResourceListSize = 3;
 
     // Points to the start of a list of resource names.
     extern const char* namedResourceList[];
